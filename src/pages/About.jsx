c5dcb940b303d373a202c2642c00dab422cdd844
unknown <you@example.com>
2023-12-22 19:36:09 +0600

    create todo page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,40 +1,37 @@
 import { BsGraphUpArrow } from "react-icons/bs";
 import { RiHotelFill } from "react-icons/ri";
-
+import pic from "../assets/6269678.jpg"
+import img from "../assets/demo_logo.png"
 const About = () => {
     return (
         <div
             data-aos="fade-up"
             data-aos-duration="1500"
-            className="max-w-7xl mx-auto my-12 flex gap-12 py-16 items-center px-5 flex-col md:flex-row"
+            className="max-w-7xl mx-auto flex gap-12 py-16 items-center px-5 flex-col md:flex-row"
         >
             <div className="border border-dark-03 p-3 relative w-full md:w-2/5 lg:w-1/2 mr-12 ml-5 md:mr-0">
-                <img src="https://i.ibb.co/rM2hN2k/luxery.webp" alt="" />
-                {/* <div className="absolute -bottom-10 -right-10 w-56 md:w-60 lg:w-80 p-3 bg-white">
+                <img src={pic} alt="" />
+                <div className="absolute -bottom-10 -right-10 w-56 md:w-60 lg:w-80 p-3 bg-white">
                     <img
-                        src="https://www.technovisionbd.com/wp-content/themes/tet/assets/images/logo.png"
+                        src={img}
                         alt=""
                     />
-                </div> */}
+                </div>
             </div>
             <div className="w-full md:w-3/5 lg:w-1/2 mt-6">
                 <h3 className="text-dark-03 tracking-[5px] mb-3">
-                    WELCOME TO Technovision
+                    Welcome to our Task Management Site
                 </h3>
                 <h2 className="text-2xl text-dark-01 md:text-3xl lg:text-4xl font-medium mb-2">
-                    CCTV CAMERA IN BANGLADESH | BENEFITS OF SURVEILLANCE CAMERA
+                    CCTV CAMERA IN BANGLADESH
                 </h2>
                 <div className="w-20 h-1.5 bg-dark-03 mb-5 ml-2"></div>
                 <p className="text-dark-02 mb-8">
-                    {"We're"} Time –Attendance and Leave management enable to track record
-                    from any brands proximity or bio-metric (Fingerprint or Face
-                    recognition) devices. It’s agent system send the records to cloud that
-                    enables you to monitor the attendance and leave status in real time
-                    from anywhere anytime in the globe.
+                    {"We're"} where you can efficiently organize, monitor, and track your tasks and projects. This intuitive dashboard provides a comprehensive overview of your ongoing jobs, deadlines, and team activities. Streamline your workflow and enhance collaboration with our powerful features.
                 </p>
                 <div className="flex flex-wrap gap-6 md:gap-2 justify-between items-center mb-5 flex-col md:flex-row">
                     <div className="flex gap-4 items-center">
-                        <div className="text-[#5CE1E6] text-4xl">
+                        <div className="text-blue-500 text-4xl">
                             <BsGraphUpArrow></BsGraphUpArrow>
                         </div>
                         <div>
@@ -47,7 +44,7 @@ const About = () => {
                         </div>
                     </div>
                     <div className="flex gap-4 items-center">
-                        <div className="text-[#5CE1E6] text-5xl">
+                        <div className="text-blue-500 text-5xl">
                             <RiHotelFill></RiHotelFill>
                         </div>
                         <div>
--- src/pages/Dashboard/List.jsx
+++ src/pages/Dashboard/List.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import ManageTaskCard from "../../components/DashboardForm/ManageTaskCard";
 
-const List = () => {
+const ManageTask = () => {
+    const axiosPublic = useAxiosPublic()
+    const { data: tasks = [], isLoading } = useQuery({
+        queryKey: ['tasks'],
+        queryFn: async () => {
+            try {
+                const res = await axiosPublic.get(`/api/v1/show-all-task/${user?.email}`);
+                if (res.data) {
+
+                    return res.data;
+                }
+
+
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+
+            }
+        },
+    });
     return (
         <div>
+            <div className="border-2 border-[#7345b2] flex justify-around ">
+                <h2 className="font-bold">To do list</h2>
+                <h2 className="font-bold">On Going</h2>
+                <h2 className="font-bold">Completed</h2>
+                {/* className="grid grid-cols-1 w-64 gap-5  mt-2" */}
+            </div>
 
+            <div className="grid grid-cols-1 w-64 gap-5  mt-2">
+                {tasks.map((item, index) => (
+                    <ManageTaskCard key={index} data={item}></ManageTaskCard>
+                ))}
+            </div>
         </div>
     );
 };
 
-export default List;
+export default ManageTask;
